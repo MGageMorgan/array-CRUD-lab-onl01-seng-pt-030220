b32af7a9dd10a88e42d1e3988cb0f3c39ebef93a
Gage Morgan <gage@gages.blog>
2020-03-07 02:50:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array.unshift("arrays!")
+  array[4]
 end
 
 def remove_element_from_start_of_array(array)
